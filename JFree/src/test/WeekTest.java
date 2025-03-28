5e15abaa6a218da1a1c0426bdddbdbc7c87ade10
AsmaaElawady <asmaalwdy6@gmail.com>
2025-03-28 19:52:12 +0200

    Added some test Methods in Week test

--- JFree/src/test/WeekTest.java
+++ JFree/src/test/WeekTest.java
@@ -4,6 +4,7 @@ import org.jfree.data.time.Week;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import org.jfree.data.time.Year;
 
 public class WeekTest {
     Week week;
@@ -16,6 +17,30 @@ public class WeekTest {
         arrange();
         assertEquals(2025, week.getYear().getYear());
         assertEquals(12, week.getWeek()); // to be updated with the current week
+    }
+
+    @Test
+    public void testGetYear() {
+        // Arrange
+        Week week = new Week(15, 2026);
+
+        // Act
+        Year year = week.getYear();
+
+        // Assert
+        assertEquals(2026, year.getYear());
+    }
+
+    @Test
+    public void testGetWeek() {
+        // Arrange
+        Week week = new Week(22, 2027);
+
+        // Act
+        int weekNumber = week.getWeek();
+
+        // Assert
+        assertEquals(22, weekNumber);
     }
 
 
